0663fb0cffc0291070521d1f6a793009922e3964
Aguilar Marquez <aguilhm1@jhuapl.edu>
2021-01-20 16:03:47 -0500

    added xplane udp

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Import XPlane class from the package
+//import { XPlaneClient } from "xplane-udp"
+var XPlaneClient = require('xplane-udp');
+
 var expri1 = require('./final.js'); 
 var dgram = require('dgram');
 const final = require('./final.js');
@@ -11,6 +15,40 @@ console.log('Server-side code running');
 const express = require('express');
 
 const app = express();
+
+
+
+
+
+// Create a client on a specific port (this is a value you define in your xplane game settings)
+const client2 = new XPlaneClient(49100)
+// Start the UDP listener
+client2.start()
+
+// Subscribe to the updated event
+// This will fire at the rate at which you send UDP updates (set in-game)
+client2.on("updated", (data) => {
+    
+    // Do something with the data
+    console.log(data)
+    callMeMaybe()
+
+})
+
+// You can also access the data directly from the client instance
+callMeMaybe(() => {
+    console.log("call me maybe has been called")
+    // Access data in the following format `<client>.data.<group>.<value>`
+    console.log(client2.data.time.real)
+    console.log(client2.data.airspeed.indicated)
+
+})
+
+
+
+
+
+
 
 // serve files from the public directory
 app.use(express.static('public'));
